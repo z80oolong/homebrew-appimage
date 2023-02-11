f145a7d1e2f9b93e294e87b602cb8a773dafd8b1
NAKATSUKA, Yukitaka <zool@zool.jpn.org>
2023-02-12 02:39:05 +0900

    Fix glibc prolem.

--- cmd/lib/shellcommands.rb
+++ cmd/lib/shellcommands.rb
@@ -6,41 +6,43 @@ module AppImage
     include Singleton
 
     def initialize
-      @patchelf ||= (Formula["patchelf"].opt_bin/"patchelf")
+      @patchelf     ||= (Formula["patchelf"].opt_bin/"patchelf")
       @appimagetool ||= (Formula["z80oolong/appimage/appimagetool"].opt_bin/"appimagetool")
-      @runtime ||= (Formula["z80oolong/appimage/appimage-runtime"].opt_libexec/"runtime-x86_64")
-      @curl ||= Pathname.new(%x{which curl}.chomp!)
-      @sha256sum ||= Pathname.new(%x{which sha256sum}.chomp!)
-      @gpg2 ||= Pathname.new(%x{which gpg}.chomp!)
-      @ldd ||= Pathname.new(%x{which ldd}.chomp!)
-      @cp ||= Pathname.new(%x{which cp}.chomp!)
-      @ln ||= Pathname.new(%x{which ln}.chomp!)
+      @runtime      ||= (Formula["z80oolong/appimage/appimage-runtime"].opt_libexec/"runtime-x86_64")
+      @ldd          ||= (Formula["glibc"].opt_bin/"ldd")
+      @curl         ||= Pathname.new(%x{which curl}.chomp!)
+      @sha256sum    ||= Pathname.new(%x{which sha256sum}.chomp!)
+      @gpg2         ||= Pathname.new(%x{which gpg}.chomp!)
+      @cp           ||= Pathname.new(%x{which cp}.chomp!)
+      @ln           ||= Pathname.new(%x{which ln}.chomp!)
 
-      unless @patchelf.executable? then
-        system "brew reinstall -s patchelf"
+      if !@patchelf.executable? then
+        system "brew", "reinstall", "-s", "patchelf"
+        @patchelf = (Formula["patchelf"].opt_bin/"patchelf")
       end
 
-      unless @appimagetool.executable? then
-        system "brew reinstall appimagetool"
+      if !@appimagetool.executable? then
+        system "brew", "reinstall", "appimagetool"
+        @appimagetool = (Formula["z80oolong/appimage/appimagetool"].opt_bin/"appimagetool")
       end
 
-      unless @runtime.executable? then
-        system "brew reinstall appimage-runtime"
+      if !@runtime.executable? then
+        system "brew", "reinstall", "appimage-runtime"
+        @runtime = (Formula["z80oolong/appimage/appiamge-runtime"].opt_libexec/"runtime-x86_64")
       end
 
-      unless @curl.executable? then
-        system "brew reinstall curl"
+      if !@curl.executable? then
+        system "brew", "reinstall", "curl"
         @curl = (Formula["curl"].opt_bin/"curl")
       end
 
-      unless @gpg2.executable? then
-        system "brew reinstall gpg2"
+      if !@gpg2.executable? then
+        system "brew", "reinstall", "gpg2"
         @gpg2 = (Formula["gpg2"].opt_bin/"gpg")
       end
 
-      unless @ldd.executable? then
-        system "brew reinstall glibc"
-        @ldd = (Formula["glibc"].opt_bin/"ldd")
+      if !@ldd.executable? then
+        @ldd ||= Pathname.new(%x{which ldd}.chomp!)
       end
 
       unless (@sha256sum.executable? || @cp.executable? || @ln.executable?) then
